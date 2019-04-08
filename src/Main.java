7595cdc600ec9e7eba8179f244ba828766a0c1a6
Crow314 <crow31415@gmail.com>
2019-04-09 03:01:19 +0900

    AC

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+
+        System.out.print("  ");
+
+        for(int i=1; i<10; i++) {
+            System.out.print(" ");
+            System.out.printf("%02d", i);
+        }
+
+        System.out.println();
+
+        for(int i=1; i<10; i++) {
+            System.out.printf("%02d", i);
+            for(int j=1; j<10; j++) {
+                System.out.print(" ");
+                System.out.printf("%02d", i*j);
+            }
+            System.out.println();
+        }
+
+
     }
+
 }
