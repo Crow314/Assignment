35c033daced5358455c08e89aa9a30492974a6bf
Crow314 <crow31415@gmail.com>
2019-04-09 02:58:14 +0900

    AC

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+        Scanner sc = new Scanner(System.in);
+
+        double height = 0;
+        do{
+            System.out.println("Height:");
+            height = sc.nextDouble();
+        }while (!(height>=100 && height<200));
+
+        double weight = 0;
+
+        do{
+            System.out.println("Weight:");
+            weight = sc.nextDouble();
+        }while (!(weight>=10 && weight<200));
+
+        sc.close();
+
+        double heightDouble = height / 100;
+
+        double BMI = weight / (heightDouble * heightDouble);
+        System.out.println("Your BMI is " + BMI);
     }
 }
