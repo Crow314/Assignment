5f3939d56246408627005da169e29c6dd112f945
Crow314 <crow31415@gmail.com>
2019-04-09 03:40:34 +0900

    AC

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("N:");
+        int N = sc.nextInt();
+
+        Set<Integer> submitList = new TreeSet<>();
+        for(int i=1; i<=N; i++){
+            submitList.add(i);
+        }
+
+        while (true){
+            int in = sc.nextInt();
+
+            if(in == 99) {
+                break;
+            }
+
+            submitList.remove(in);
+        }
+
+        Integer[] out = submitList.toArray(new Integer[0]);
+
+        System.out.println("Result");
+        for(int i=0; i<out.length; i++){
+            System.out.println(out[i]);
+        }
+
     }
 }
