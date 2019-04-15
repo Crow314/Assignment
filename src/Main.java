005afc0048f3c82a4d8bd0b93e31fb2894e227c3
Crow314 <crow31415@gmail.com>
2019-04-15 13:13:07 +0900

    AC

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        sc.close();
+
+        isLeapYear(year);
+
+    }
+
+    private static void isLeapYear(int year){
+        boolean leap = false;
+
+        if(year%400 == 0){
+            leap = true;
+        }else if(year%100 == 0){
+            leap = false;
+        }else if(year%4 == 0){
+            leap = true;
+        }
+
+        if(leap){
+            System.out.println("Year " + year + " is a leap year!");
+        }else{
+            System.out.println("Year " + year + " is NOT a leap year.");
+        }
     }
 }
