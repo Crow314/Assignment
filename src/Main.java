cdf63fcac164a30ad371a7554f34e0a8f0e97e0a
Crow314 <crow31415@gmail.com>
2019-04-15 13:48:17 +0900

    問題定義準拠(配布ファイル対応)

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        //5科目の点数を入力し，配列scoreに格納
+        int[] score = input(5);
 
-        int sum = 0;
+        //合計を計算し，表示
+        int total = total(score);
+        System.out.println("Total:" + total);
 
-        for(int i=0; i<5; i++){
-            sum += sc.nextInt();
-        }
-        sc.close();
-
-        double ave = (double)sum / 5;
-
-        System.out.println("Total:" + sum);
-        System.out.println("Average:" + ave);
-
+        //平均点を計算し，表示
+        double average = average(score);
+        System.out.println("Average:" + average);
     }
+
+    private static int[] input(int kamoku) {
+        int[] list = new int[kamoku];
+
+        Scanner in = new Scanner(System.in);
+        for(int i=0; i<kamoku; i++)
+            list[i] = in.nextInt();
+
+        return list;
+    }
+
+    private static int total(int[] score){
+        int result = 0;
+        for(int i=0; i<score.length; i++){
+            result += score[i];
+        }
+        return result;
+    }
+
+    private static double average(int[] score){
+        double result = 0.0;
+        for(int i=0; i<score.length; i++){
+            result += score[i];
+        }
+
+        result /= score.length;
+
+        return result;
+    }
+
 }
