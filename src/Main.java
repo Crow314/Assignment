cba73f404fc2eaaab55d8bd6e9f10ae97c505026
Crow314 <crow31415@gmail.com>
2019-04-15 13:42:06 +0900

    AC

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,21 @@
 public class Main {
-
     public static void main(String[] args) {
-
-        System.out.print("  ");
-
-        for(int i=1; i<10; i++) {
-            System.out.print(" ");
-            System.out.printf("%02d", i);
-        }
-
-        System.out.println();
-
-        for(int i=1; i<10; i++) {
-            System.out.printf("%02d", i);
-            for(int j=1; j<10; j++) {
-                System.out.print(" ");
-                System.out.printf("%02d", i*j);
-            }
+        System.out.println("   01 02 03 04 05 06 07 08 09");
+        for(int i=1; i<10; i++){
+            System.out.print("0" + i);
+            lineOutput(i);
             System.out.println();
         }
-
-
     }
 
+    private static void lineOutput(int y) {
+        for (int x = 1; x < 10; x++) {
+            int n = x * y;
+            // 1桁の時の追加処理
+            if (n < 10)
+                System.out.print(" 0" + n);
+            else
+                System.out.print(" " + n);
+        }
+    }
 }
