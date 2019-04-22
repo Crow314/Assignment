62384556c5f6901089e922d4f09de678676cc164
Crow314 <crow31415@gmail.com>
2019-04-22 13:24:13 +0900

    AC

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,64 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+        Scanner sc = new Scanner(System.in);
+
+        String pass = sc.next();
+
+        boolean result = isGoodPass(pass);
+
+        String res_out = result ? "OK" : "NG";
+
+        System.out.println("Password " + res_out);
+    }
+
+    public static boolean isGoodPass(String pass){
+        if(!isInRange(pass, 4, 6)){
+            return false;
+        }
+        if(!hasLowerCase(pass)){
+            return false;
+        }
+        if(!hasUpperCase(pass)){
+            return false;
+        }
+        if(!hasNumeric(pass)){
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isInRange(String pass, int min, int max){
+        int length = pass.length();
+        if(min <= length && max >= length){
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean hasLowerCase(String pass){
+        return hasCharInRange(pass, 'a', 'z');
+    }
+
+    private static boolean hasUpperCase(String pass){
+        return hasCharInRange(pass, 'A', 'Z');
+    }
+
+    private static boolean hasNumeric(String pass){
+        return hasCharInRange(pass, '0', '9');
+    }
+
+    private static boolean hasCharInRange(String pass, char start, char fin){
+        char[] passCharArray = pass.toCharArray();
+
+        for(int i=0; i<passCharArray.length; i++){
+            char tmp = passCharArray[i];
+            if(tmp >= start&& tmp <= fin){
+                return true;
+            }
+        }
+        return false;
     }
 }
