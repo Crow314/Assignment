d8820adf54d0eca8df44c46573253ec501c20154
Crow314 <crow31415@gmail.com>
2019-05-13 13:16:00 +0900

    AC

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+        Scanner sc = new Scanner(System.in);
+
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+        Rect rect = new Rect(w, h);
+
+        System.out.println("The rectangle has " + rect.getHeight() + " height, " + rect.getWidth() + " width and " + rect.getArea() + " area.");
+    }
+}
+
+class Rect{
+    private int width;
+    private int height;
+    private int area;
+
+    Rect(int w, int h){
+        this.width = w;
+        this.height = h;
+        this.area = w * h;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getArea() {
+        return area;
     }
 }
