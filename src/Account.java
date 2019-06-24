ca159dda38cb49099c2ede2acf8eeb3da1692841
Crow314 <crow31415@gmail.com>
2019-06-24 13:18:23 +0900

    AC

---
+++ src/Account.java
@@ -0,0 +1,33 @@
+public class Account {
+
+    private String name;
+    private String no;
+    private int balance;
+
+    Account(String name, String no){
+        this.name = name;
+        this.no = no;
+        this.balance = 0;
+    }
+
+    private boolean isSufficientAmount(int amount){
+        return (balance >= amount);
+    }
+
+    public void deposit(int amount){
+        balance += amount;
+    }
+
+    public boolean withdraw(int amount){
+        if(!isSufficientAmount(amount)){
+            return false;
+        }
+        balance -= amount;
+        return true;
+    }
+
+    @Override
+    public String toString(){
+        return name + "(" + no + ") :" + balance;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+
+        Account account = new Account(inputString("name"), inputString("no"));
+
+        while(true){
+            System.out.println(account);
+            account.deposit(inputInt("deposit amount"));
+            System.out.println(account.withdraw(inputInt("withdraw amount")));
+
+        }
     }
+
+    private static String inputString(String message){
+        Scanner sc = new Scanner(System.in);
+        System.out.print(message + ":");
+        return sc.next();
+    }
+
+    private static int inputInt(String message){
+        Scanner sc = new Scanner(System.in);
+        System.out.print(message + ":");
+        return sc.nextInt();
+    }
+
 }
