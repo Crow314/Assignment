2971546cd146bb1bd6c8919a271a15d68089d1d3
Crow314 <crow31415@gmail.com>
2019-10-09 16:26:24 +0900

    AC

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        sc.close();
+
+        int result = X * 1000 - A - B * C;
+        result %= 1000;
+        System.out.println(result);
     }
 }
