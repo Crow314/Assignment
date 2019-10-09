a6465ab21346527e18bd336c6e9710c7f61da9d6
Crow314 <crow31415@gmail.com>
2019-10-09 15:07:39 +0900

    AC

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
+
+        try{
+            for(int i=0; i<2; i++){
+                result += sc.nextInt();
+            }
+            System.out.println("result:" + result);
+        }catch (Exception e){
+            System.out.println("Invalid Input!");
+        }
     }
 }
