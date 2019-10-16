10940df68e79c2a4d5ba9ebb920a3bd73fc4340e
Crow314 <master@crow31415.net>
2019-10-16 14:57:44 +0900

    AC

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int result = 0;
+        List<Integer> input = new ArrayList<>();
 
         try{
-            for(int i=0; i<2; i++){
-                result += sc.nextInt();
+            while(true){
+                input.add(sc.nextInt());
             }
-            System.out.println("result:" + result);
         }catch (Exception e){
             System.out.println("Invalid Input!");
         }
+
+        int result = 0;
+        System.out.print("input: ");
+
+        if(input.size()%2 == 0){
+            for(int i=0; i<input.size(); i++){
+                System.out.print(" " + input.get(i));
+                result += input.get(i);
+            }
+        }else{
+            result = 1;
+            for(int i=0; i<input.size(); i++){
+                System.out.print(" " + input.get(i));
+                result *= input.get(i);
+            }
+        }
+
+        System.out.println();
+        System.out.println("result:" + result);
     }
 }
