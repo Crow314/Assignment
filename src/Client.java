c3e5c8ceea812833e7d751c7f8299628537a3446
Crow314 <master@crow31415.net>
2019-11-20 15:51:53 +0900

    Create

--- src/Client.java
+++ src/Client.java
@@ -1,15 +1,25 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         try(
                 Socket sock = new Socket("127.0.0.1", 65432)
         ) {
             BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
             PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+            System.out.println("127.0.0.1:65432へ接続開始");
 
-            out.println("はろー");
+            System.out.println("送る行数:");
+            int dataLength = sc.nextInt();
+            out.println(dataLength);
+
+            for(int i=0; i<dataLength; i++){
+                out.println(sc.next());
+            }
+
             String result = in.readLine();
             System.out.println("サーバからの返信: " + result);
         }catch (IOException e){
--- src/Server.java
+++ src/Server.java
@@ -4,18 +4,37 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
-        try(
-                ServerSocket servSock = new ServerSocket(65432);
-                Socket sock = servSock.accept()
-                ) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+        while (true){
+            try(
+                    ServerSocket servSock = new ServerSocket(65432);
+                    Socket sock = servSock.accept()
+            ) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
 
-            String data = in.readLine();
-            System.out.println("受信データ: " + data);
-            out.println("データ受け取りました");
-        }catch (IOException e){
-            e.printStackTrace();
+                System.out.println("接続の受付を開始します。");
+                String data = in.readLine();
+
+                int dataLength = Integer.valueOf(data);
+
+                String[] dataArray = new String[dataLength];
+
+                for(int i=0; i<dataLength; i++){
+                    dataArray[i] = in.readLine();
+                }
+
+                System.out.println("受信データ: " + data);
+
+                for(int i=0; i<dataLength; i++){
+                    System.out.println("受信データ: " + dataArray[i]);
+                }
+
+                out.println("データ受け取りました");
+                System.out.println("切断します");
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
 }
