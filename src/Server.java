5ae2c690e7611863ff3d895188d5a26f967e7a71
Crow314 <master@crow31415.net>
2019-11-20 15:53:19 +0900

    Serverは止まらない

--- src/Server.java
+++ src/Server.java
@@ -4,18 +4,20 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
-        try(
-                ServerSocket servSock = new ServerSocket(65432);
-                Socket sock = servSock.accept()
-                ) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+        while (true){
+            try(
+                    ServerSocket servSock = new ServerSocket(65432);
+                    Socket sock = servSock.accept()
+            ) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
 
-            String data = in.readLine();
-            System.out.println("受信データ: " + data);
-            out.println("データ受け取りました");
-        }catch (IOException e){
-            e.printStackTrace();
+                String data = in.readLine();
+                System.out.println("受信データ: " + data);
+                out.println("データ受け取りました");
+            }catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
 }
