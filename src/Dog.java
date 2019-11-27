7783f668ca44de3a1d300257e802f8494c569f8e
Crow314 <master@crow31415.net>
2019-11-27 15:55:56 +0900

    Create

---
+++ src/Dog.java
@@ -0,0 +1,56 @@
+public class Dog extends Thread {
+
+    private String name;
+    private double stepLength;
+    private int stepInterval;
+
+    public Dog(String name, double stepLength, int stepInterval){
+        this.name = name;
+        this.stepLength = stepLength;
+        this.stepInterval = stepInterval;
+    }
+
+    @Override
+    public void run() {
+        double distance = 0;
+        int goalDistance = 600;
+        int[] checkPoint = {200, 400};
+        int checkPointCount = 0;
+        int remainGrateStatus = 0;
+
+        while (distance < goalDistance){
+            if(remainGrateStatus>0){
+                distance += stepLength*2;
+                remainGrateStatus--;
+
+                if(remainGrateStatus == 0){
+                    System.out.println(name + "は絶好調じゃなくなった");
+                }
+            }else{
+                distance += stepLength;
+            }
+
+            if(checkPointCount<checkPoint.length){
+                if(distance >= checkPoint[checkPointCount]){
+                    System.out.println(name + "が" + checkPoint[checkPointCount] + "mに到達!");
+                    checkPointCount++;
+                }
+            }
+
+            int random = (int)(Math.random() * 100);
+            if(random == 0 && remainGrateStatus == 0){
+                remainGrateStatus = 10;
+                System.out.println(name + "は絶好調!!");
+            }
+
+            try {
+                Thread.sleep(stepInterval);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(name + "がゴール!!");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+        Scanner sc = new Scanner(System.in);
+
+        Dog[] dogs = new Dog[4];
+
+        for(int i=0; i<dogs.length; i++){
+            System.out.print("Input name of the dog. :");
+            String tmpName = sc.next();
+            System.out.print("Input step length of the dog. :");
+            double tmpStepLength = sc.nextDouble();
+            System.out.print("Input step interval of the dog. :");
+            int tmpStepInterval = sc.nextInt();
+            dogs[i] = new Dog(tmpName, tmpStepLength, tmpStepInterval);
+        }
+
+        for (Dog dog : dogs) {
+            dog.start();
+        }
     }
 }
